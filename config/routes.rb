a1a638428bac907f885023194e2be22c6152d2d2
worldask <worldask@gmail.com>
2014-08-22 12:50:44 +0800

    添加路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,35 @@
 Rails.application.routes.draw do
+  root 'index#index'
+  get '/signin' => 'signin#index'
+
+  #scope 'admin', :as => 'admin' do
+  namespace :admin do
+    get '/' => 'admin_signin#index'
+    get '/signin' => 'admin_signin#index'
+    post '/signin' => 'admin_signin#signin'
+    get '/signout' => 'admin_signin#signout'
+    get '/welcome' => 'welcome#index'
+    get '/menu' => 'admin_modules#index'
+
+    get '/roles/:id/get_modules' => 'admin_roles#get_modules'
+    post '/roles/:id/set_modules' => 'admin_roles#set_modules'
+
+    get '/modules/tree' => 'admin_modules#tree'
+
+    concern :search do
+      get 'search', on: :collection
+    end
+    concern :bulk_delete do
+      delete 'list', on: :collection
+    end
+    concern :is_unique do
+      get 'list', on: :collection
+    end
+
+    resources :users, :controller => 'admin_users', concerns: [:search, :bulk_delete, :is_unique]
+    resources :depts, :controller => 'admin_depts', concerns: [:search, :bulk_delete, :is_unique]
+    resources :roles, :controller => 'admin_roles', concerns: [:search, :bulk_delete, :is_unique]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
