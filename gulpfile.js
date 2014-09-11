81b3a391f603f4b9881a9fa7fb4ede4adca38ebb
worldask <worldask@gmail.com>
2014-09-11 15:23:32 +0800

    gulp调整

--- gulpfile.js
+++ gulpfile.js
@@ -13,40 +13,44 @@ var paths = {
   js: ['app/assets/javascripts/*.js'],
   scss: ['app/assets/stylesheets/*.scss'],
   css: ['app/assets/stylesheets/*.css'],
-  js_admin: ['app/assets/javascripts/admin/*.js'],
+  dest_dev: 'app/assets',
+  dest: 'public/assets',
+  js_admin: 'app/assets/javascripts/admin/*.js',
   scss_admin: ['app/assets/stylesheets/admin/*.scss'],
   css_admin: ['app/assets/stylesheets/admin/*.css'],
-  dest: 'public/assets'
+  dest_admin_dev: 'app/assets/admin',
+  dest_admin: 'public/assets/admin'
 };
 
 gulp.task('clean', function(cb) {
    del(paths.dest, cb);
 });
-    
+
 gulp.task('vendor_js', function () {
   return gulp.src([
-    'vendor/assets/bower_components/angular/angular.js',
-    'vendor/assets/bower_components/angular-animate/angular-animate.js',
-    'vendor/assets/bower_components/angular-route/angular-route.js',
-    'vendor/assets/bower_components/jquery/src/jquery.js',
-    'vendor/assets/bower_components/bootstrap/dist/js/bootstrap.js',
-    'vendor/assets/bower_components/angular/angular.js',
-    ])
-             .pipe(changed(paths.dest))
-             .pipe(uglify())
+    'vendor/assets/bower_components/jquery/dist/jquery.min.js',
+    'vendor/assets/bower_components/angular/angular.min.js',
+    'vendor/assets/bower_components/angular-animate/angular-animate.min.js',
+    'vendor/assets/bower_components/angular-route/angular-route.min.js',
+    'vendor/assets/bower_components/bootstrap/dist/js/bootstrap.min.js',
+  ])
+             .pipe(changed(paths.dest_admin))
+             //.pipe(ngmin())
              .pipe(concat('vendor.js'))
-             .pipe(gulp.dest(paths.dest));
+             .pipe(gulp.dest(paths.dest_admin_dev))
+             .pipe(gulp.dest(paths.dest_admin));
 });
 
 gulp.task('vendor_css', function () {
   return gulp.src([
-    'vendor/assets/bower_components/bootstrap/dist/css/bootstrap.css',
-    'vendor/assets/bower_components/font-awesome/css/font-awesome.css',
+    'vendor/assets/bower_components/bootstrap/dist/css/bootstrap.min.css',
+    'vendor/assets/bower_components/font-awesome/css/font-awesome.min.css',
     ])
-             .pipe(changed(paths.dest))
-             .pipe(minify_css())
+             .pipe(changed(paths.dest_admin))
+             //.pipe(minify_css())
              .pipe(concat('vendor.css'))
-             .pipe(gulp.dest(paths.dest));
+             .pipe(gulp.dest(paths.dest_admin_dev))
+             .pipe(gulp.dest(paths.dest_admin));
 });
 
 gulp.task('vendor_fonts', function () {
@@ -56,8 +60,10 @@ gulp.task('vendor_fonts', function () {
     'vendor/assets/bower_components/font-awesome/fonts/fontawesome-webfont.ttf',
     'vendor/assets/bower_components/font-awesome/fonts/fontawesome-webfont.svg'
     ])
-             .pipe(changed(paths.dest))
-             .pipe(gulp.dest(paths.dest));
+             .pipe(changed('app/assets/fonts'))
+             .pipe(changed('public/assets/fonts'))
+             .pipe(gulp.dest('app/assets/fonts'))
+             .pipe(gulp.dest('public/assets/fonts'));
 });
 
 gulp.task('js', ['clean'], function () {
@@ -68,6 +74,7 @@ gulp.task('js', ['clean'], function () {
               .pipe(uglify())
              .pipe(ngmin())
              .pipe(concat('app.js'))
+             .pipe(gulp.dest(paths.dest_dev))
              .pipe(gulp.dest(paths.dest));
 });
 
@@ -87,31 +94,46 @@ gulp.task('css', ['clean', 'scss'], function () {
              .pipe(gulp.dest(paths.dest));
 });
 
-gulp.task('js_admin', ['clean'], function () {
-  return gulp.src(paths.js_admin)
-             .pipe(changed(paths.dest))
+gulp.task('js_admin_app', ['clean'], function () {
+  return gulp.src([
+    'app/assets/javascripts/admin/base.js',
+    'app/assets/javascripts/admin/util.js'
+              ])
+             .pipe(changed(paths.dest_admin))
              .pipe(jshint())
              //.pipe(jshint.reporter('js'))
               .pipe(uglify())
              .pipe(ngmin())
-             .pipe(concat('app_admin.js'))
-             .pipe(gulp.dest(paths.dest));
+             .pipe(concat('app.js'))
+             .pipe(gulp.dest(paths.dest_admin_dev))
+             .pipe(gulp.dest(paths.dest_admin));
+});
+gulp.task('js_admin', ['clean'], function () {
+  return gulp.src([paths.js_admin, 
+    '!app/assets/javascripts/admin/base.js',
+    '!app/assets/javascripts/admin/util.js'
+    ])
+             .pipe(changed(paths.dest_admin))
+             .pipe(jshint())
+              .pipe(uglify())
+             .pipe(ngmin())
+             .pipe(gulp.dest(paths.dest_admin_dev))
+             .pipe(gulp.dest(paths.dest_admin));
 });
 
 gulp.task('scss_admin', function () {
   return gulp.src(paths.scss_admin)
-             .pipe(changed(paths.dest))
              .pipe(sass())
              .pipe(gulp.dest('app/assets/stylesheets/admin'));
 });
 
-gulp.task('css_admin', ['clean', 'scss_admin'], function () {
+gulp.task('css_admin', ['scss_admin'], function () {
   return gulp.src(paths.css_admin)
 //             .pipe(changed(paths.dest))
              .pipe(sass())
              .pipe(minify_css())
-             .pipe(concat('app_admin.css'))
-             .pipe(gulp.dest(paths.dest));
+             .pipe(concat('app.css'))
+             .pipe(gulp.dest(paths.dest_admin));
 });
 
 gulp.task('watch', function() {
@@ -119,4 +141,5 @@ gulp.task('watch', function() {
   gulp.watch(paths.css_admin, ['css_admin']);
 });
 
-gulp.task('default', ['vendor_js', 'vendor_css', 'vendor_fonts', 'js', 'scss', 'css', 'js_admin', 'scss_admin', 'css_admin']);
+//gulp.task('default', ['vendor_js', 'vendor_css', 'vendor_fonts', 'js', 'scss', 'css', 'js_admin', 'scss_admin', 'css_admin']);
+gulp.task('default', ['vendor_js', 'vendor_css', 'vendor_fonts',  'js_admin_app', 'js_admin', 'scss_admin', 'css_admin']);
