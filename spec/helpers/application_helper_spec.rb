984e202a898f8f7d7fcd94414b08bd79af6d9e41
worldask <worldask@gmail.com>
2014-09-16 16:06:34 +0800

    add spec helper

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe ApplicationHelper do
+  let(:uri) { "name=abc&type=eq=1&color=in=red,yellow,blue&remark=lk=good&_st=-count&" }
+
+  describe "#get_sort_pattern" do
+    let(:sql_sort) { "ORDER BY count DESC" }
+
+    it "sort patten" do
+      patten = get_sort_pattern Rack::Utils.parse_query(uri)
+      expect(patten).to eq(sql_sort)
+    end
+  end
+
+  describe '#get_where_pattern' do
+    let(:sql_where) { ["name = 'abc'", "type = '1'", "color IN ('red', 'yellow', 'blue')", "remark LIKE %good%"] }
+
+    it "where pattern" do
+      pattern = get_where_pattern Rack::Utils.parse_query(uri)
+      expect(pattern).to eq(sql_where.join(" AND "))
+    end
+
+    it "where pattern with OR" do
+      pattern = get_where_pattern Rack::Utils.parse_query(uri << "&_rel=OR")
+      expect(pattern).to eq(sql_where.join(" OR "))
+    end
+  end
+end
