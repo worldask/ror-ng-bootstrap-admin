67f4df1087476470b0f4623102a2ac0eab374df6
worldask <worldask@gmail.com>
2014-09-17 12:54:25 +0800

    remove BaseListController in html; convert BaseListController js to a factory named list; reformat list.js

--- app/assets/javascripts/admin/admin_depts.js
+++ app/assets/javascripts/admin/admin_depts.js
@@ -1,3 +1,4 @@
 // 后台部门管理
-app.controller('Admin::AdminDeptsController', ['$scope', '$http', function($scope, $http) {
+app.controller('Admin::AdminDeptsController', ['$scope', '$http', '$element', 'list', function($scope, $http, $element, list) {
+  list.extend($scope, $element);
 }]);
