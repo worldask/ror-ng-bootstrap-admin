b7ce5b24021462d7b6b4781b569da26a393a2678
worldask <worldask@gmail.com>
2014-10-01 01:56:08 +0800

    move crud into common.rb in spec

--- spec/features/admin/admin_depts_spec.rb
+++ spec/features/admin/admin_depts_spec.rb
@@ -4,76 +4,13 @@ require 'rails_helper'
 feature AdminDept, :type => :feature, :js => true do
   before(:all) do
     @keyword_part = 'dept'
+    @field1 = 'name'
     @data = [
-      @row1 = { :name => "tech #{@keyword_part}" },
-      @row2 = { :name => "op #{@keyword_part}" },
+      @row1 = { @field1.to_sym => "#{@keyword_part} row 1" },
+      @row2 = { @field1.to_sym => "#{@keyword_part} row 2" },
     ]
   end
 
-    it_behaves_like 'shared_feature_common', '/admin/depts' do
-      # search
-      let(:hash_array)               { @data }
-      let(:keyword_part)             { @keyword_part }
-      let(:keyword_part_match_count) { 2 }
-      let(:keyword_whole)            { @row1[:name] }
-      let(:add_field)            { 'id="name"' }
-      let(:add_value)            { '新部门' }
-    end
-
-  context '编辑' do
-    before(:each) do
-      AdminDept.create! @data
-      visit admin_depts_path
-      sleep 0.1
-    end
-
-    describe '删除指定部门' do
-      it '删除之后该部门应不存在' do
-        find(:xpath, "//table/tbody/tr/td[text()='#{@row1[:name]}']/../td[@class='text-center']/a[@title='删除']").click
-        find('a[ng-click="del();"]').click
-        sleep 0.1
-
-        expect(page).to_not have_content @row1[:name]
-      end
-    end
-
-    describe '修改部门' do
-      it "部门名称应改变" do
-        find(:xpath, "//table/tbody/tr/td[text()='#{@row1[:name]}']/../td[@class='text-center']/a[@title='编辑']").click
-        find('input[id="name"]').set @row1[:name].reverse
-        find('a[title="保存"]').click
-        sleep 0.1
-
-        expect(page).to_not have_content @row1[:name]
-      end
-    end
-
-    describe '添加多个部门' do
-      it '记录条数应相符' do
-        btn_add = find('a[title="添加"]')
-        @data.each_with_index do |dept, i|
-          btn_add.click
-          find('input[id="name"]').set(dept[:name] + "copy")
-          find('a[title="保存"]').click
-          sleep 1
-        end
-
-        visit admin_depts_path
-        expect(page).to have_selector('table tbody tr', count: @data.length * 2)
-      end
-    end
-
-    #describe '删除所有部门' do
-    #  it '记录条数应为 0' do
-    #    find(:xpath, "//table/thead/tr//input[@type='checkbox']").click
-    #    find('#btnBatchDelete').click
-    #    find('a[ng-click="del();"]').click
-
-    #    sleep_until { all('table tbody tr').count <= 0 }
-    #    visit admin_depts_path
-
-    #    expect(page).to have_selector('table tbody tr', count: 0)
-    #  end
-    #end
+  it_behaves_like 'shared_feature_common', '/admin/depts' do
   end
 end
--- spec/support/shared_feature_common.rb
+++ spec/support/shared_feature_common.rb
@@ -5,25 +5,30 @@
 #  # spec/features/admin_xyzs_spec.rb
 #  describe AdminXyz
 #    context 'search' do
-#      it_behaves_like 'shared search feature examples', '/admin/xyzs' do
-#        let(:hash_array)                 { @xyzs }          # 用于创建 Model 实例的哈希表数组
-#        let(:keyword_part)             { 'part-of-word' } # 部分匹配关键字
-#        let(:keyword_part_match_count) { n }              # 部分匹配期望值
-#        let(:keyword_whole)            { 'whole-word' }   # 全匹配关键字, 期望值固定为1
+#      it_behaves_like 'shared_feature_common', '/admin/xyzs' do
+#        let(:hash_array)               { @xyzs }          # hash array for creating model
+#        let(:keyword_part)             { 'part-of-word' } # partial keyword
+#        let(:keyword_complete)         { 'whole-word' }   # complete keyword, row count should be 1
 #      end
 #    end
 #  end
 
 shared_examples 'shared_feature_common' do |path|
+  let(:hash_array)               { @data }
+  let(:keyword_part)             { @keyword_part }
+  let(:keyword_complete)         { @row1[@field1.to_sym] }
+  let(:add_field)                { [@field1] }
+  let(:add_value)                { ["new #{@keyword_part}"] }
+
   before(:each) do
     described_class.create! hash_array
     visit path
   end
 
   context 'search' do
-    describe '搜索 <keyword_whole>' do
-      it '记录条数应为 1' do
-        find(:xpath, "//input[@name='q']").set keyword_whole
+    describe 'search <keyword_complete>' do
+      it 'row count should be 1' do
+        find(:xpath, "//input[@name='q']").set keyword_complete
         find(:xpath, "//a[@ng-click='search(list.keyword);']").click
         sleep 0.1
 
@@ -31,8 +36,8 @@ shared_examples 'shared_feature_common' do |path|
       end
     end
 
-    describe '搜索不存在的记录 A026C12D-0247-4424-A524-82856876FA48' do
-      it '记录条数应为 0' do
+    describe 'search nonexistent row A026C12D-0247-4424-A524-82856876FA48' do
+      it 'row count should be 0' do
         find(:xpath, "//input[@name='q']").set 'A026C12D-0247-4424-A524-82856876FA48'
         find(:xpath, "//a[@ng-click='search(list.keyword);']").click
         sleep 0.1
@@ -41,18 +46,18 @@ shared_examples 'shared_feature_common' do |path|
       end
     end
 
-    describe '搜索 <keyword_part>' do
-      it '记录条数应为 keyword_part_match_count' do
+    describe 'search <keyword_part>' do
+      it 'row count should be <hash_array.length>' do
         find(:xpath, "//input[@name='q']").set keyword_part
         find(:xpath, "//a[@ng-click='search(list.keyword);']").click
         sleep 0.1
 
-        expect(page).to have_selector('table tbody tr', count: keyword_part_match_count)
+        expect(page).to have_selector('table tbody tr', count: hash_array.length)
       end
     end
 
-    describe '查询空字符串' do
-      it '记录数应为 <hash_array.length>' do
+    describe 'search empty string' do
+      it 'row count should be <hash_array.length>' do
         find(:xpath, "//input[@name='q']").set ''
         find(:xpath, "//a[@ng-click='search(list.keyword);']").click
         sleep 0.1
@@ -62,17 +67,80 @@ shared_examples 'shared_feature_common' do |path|
     end
   end
 
-  context 'edit' do
+  context 'cud' do
     describe 'add a row' do
       it 'row count should plus 1' do
         find('a[ng-click="edit({});"]').click
-        find("input[#{add_field}]").set add_value
+        add_field.each_with_index do |field, i|
+          find("input[id=\"#{add_field[i]}\"]").set add_value[i]
+        end
         find('a[ng-click="save();"]').click
         sleep 0.1
 
         expect(page).to have_selector('table tbody tr', count: @data.length + 1)
       end
     end
+
+    describe 'add several rows' do
+      it 'row count should be corresponding' do
+        btn_add = find('a[ng-click="edit({});"]')
+        @data.each_with_index do |row, i|
+          btn_add.click
+          add_field.each_with_index do |field, i|
+            find("input[id=\"#{add_field[i]}\"]").set row[add_field[i].to_sym] + "copy"
+          end
+          find('a[ng-click="save();"]').click
+          sleep 1
+        end
+
+        visit path 
+        expect(page).to have_selector('table tbody tr', count: @data.length * 2)
+      end
+    end
+
+    describe 'edit a row' do
+      it "field should be edited" do
+        find(:xpath, "//table/tbody/tr/td[text()='#{@row1[@field1.to_sym]}']/../td[@class='text-center']/a[@ng-click='edit(item);']").click
+        find("input[id=\"#{@field1.to_sym}\"]").set @row1[@field1.to_sym].reverse
+        find('a[ng-click="save();"]').click
+        sleep 0.1
+
+        expect(page).to_not have_content @row1[@field1.to_sym]
+      end
+    end
+
+    describe 'delete a row' do
+      it 'row count should minus 1' do
+        all('a[ng-click="delConfirm(item);"]').first.click
+        find('a[ng-click="del();"]').click
+        sleep 0.1
+        visit path 
+        expect(page).to have_selector('table tbody tr', count: @data.length - 1)
+      end
+    end 
+
+    describe 'delete a specified row' do
+      it "shouldn't exists after deleting" do
+        find(:xpath, "//table/tbody/tr/td[text()='#{@row1[@field1.to_sym]}']/../td[@class='text-center']/a[@ng-click='delConfirm(item);']").click
+        find('a[ng-click="del();"]').click
+        sleep 0.1
+
+        expect(page).to_not have_content @row1[@field1.to_sym]
+      end
+    end
+
+    #describe 'delete all' do
+    #  it 'row count should be 0' do
+    #    find(:xpath, "//table/thead/tr//input[@type='checkbox']").click
+    #    find('#btnBatchDelete').click
+    #    find('a[ng-click="del();"]').click
+
+    #    sleep_until { all('table tbody tr').count <= 0 }
+    #    visit path 
+
+    #    expect(page).to have_selector('table tbody tr', count: 0)
+    #  end
+    #end
   end
 end
 
