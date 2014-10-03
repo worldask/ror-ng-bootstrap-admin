a10843ea910bd59cb6e47f3002cf76e211034632
worldask <worldask@gmail.com>
2014-10-03 17:14:12 +0800

    move code to lib/active_record_extension.rb from admin_controller.rb

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -8,77 +8,27 @@ class Admin::AdminController < ApplicationController
 
   def list 
     data = {:title => model.title}
-    page = params[:page] == nil ? 1 : params[:page]
-
-    if (params[:keyword].nil?) 
-      count = model.where(is_deleted: 0).count
-      data[:list] = {:data => model.where(is_deleted: 0).order(id: :desc).page(params[:page]).per(4)}
-    else 
-      count = model.where(is_deleted: 0).where("name like ?", '%' +  params[:keyword] + '%').count
-      data[:list] = {:data => model.where(is_deleted: 0).where("name like ?", "%#{params[:keyword]}%").order(id: :desc).page(params[:page]).per(4)}
-      data[:list][:keyword] = params[:keyword]
-    end 
-
-    # paginator
-    data[:list][:count] = count
-    data[:list][:page] = page
-    data[:list][:per_page] = 4
-    data[:list][:last_page] = (count / 4.to_f).ceil
-    #data[:per_page] = Kaminari.config.default_per_page
-    #data[:page_count] = (count / Kaminari.config.default_per_page.to_f).ceil
+    data[:list] = model.list params
 
     render json: data
   end
 
   def create 
-    @model = model.new
-
-    params[model.model_name.singular].each {|k, v|
-      @model[k] = v
-    }
-    if (@model.valid?) 
-      @model.save
-      result = {code: 1, desc: '保存成功！', id: @model.id}
-    else
-      result = {code: -1, desc: @model.errors.messages}
-    end
-
-    render json: result
-    # search_field = this->search_field
-    # AdminLog::write(Auth::user()->username, $this->title . '添加' . $this->search_field . '=' . $item->$search_field, Request::getClientIp(), date('Y-m-d H:i:s', time()));
+    render json: model.create(params)
   end
 
   def update
-    @model = model::find(params[:id])
-
-    params[model.model_name.singular].each {|k, v|
-      @model[k] = v
-    }
-    if (@model.valid?) 
-      @model.save
-      result = {code: 1, desc: '保存成功！', id: @model.id}
-    else
-      result = {code: -1, desc: @model.errors.messages}
-    end
-
-    render json: result
-    # $search_field = $this->search_field;
-    # AdminLog::write(Auth::user()->username, $this->title . '编辑' . $this->search_field . '=' . $item->$search_field, Request::getClientIp(), date('Y-m-d H:i:s', time()));
+    render json: model.update(params)
   end
 
   def destroy 
-    @model = model::find(params[:id])
-    @model.update(:is_deleted => 1)
-
-    render json: {code: 1, desc: '删除成功！'}
+    render json: model.destroy(params)
   end
 
   def bulk_delete
-    model.where(:id => params[:ids]).update_all(:is_deleted => 1)
-
-    render json: {code: 1, desc: '删除成功！'}
+    render json: model.bulk_delete(params)
   end
-  
+
 
   layout :choose_layout
 
--- lib/active_record_extension.rb
+++ lib/active_record_extension.rb
@@ -4,11 +4,74 @@ module ActiveRecordExtension
 
   # add your static(class) methods here
   module ClassMethods
-    def list
-      order_by = 'id'
-      order_seq = 'desc'
+    def list(params)
+      data = {}
+      page = params[:page] == nil ? 1 : params[:page]
 
-      self.limit(10).offset(0).order(order_by => order_seq.to_sym)
+      if (params[:keyword].nil?) 
+        count = self.where(is_deleted: 0).count
+        data[:list] = {:data => self.where(is_deleted: 0).order(id: :desc).page(params[:page]).per(4)}
+      else 
+        count = self.where(is_deleted: 0).where("name like ?", "%#{params[:keyword]}%").count
+        data[:list] = {:data => self.where(is_deleted: 0).where("name like ?", "%#{params[:keyword]}%").order(id: :desc).page(params[:page]).per(4)}
+        data[:list][:keyword] = params[:keyword]
+      end 
+
+      # paginator
+      data[:list][:count] = count
+      data[:list][:page] = page
+      data[:list][:per_page] = 4
+      data[:list][:last_page] = (count / 4.to_f).ceil
+      #data[:per_page] = Kaminari.config.default_per_page
+      #data[:page_count] = (count / Kaminari.config.default_per_page.to_f).ceil
+
+      data[:list]
+    end
+
+    def create(params)
+      @item = self.new
+
+      params[self.model_name.singular].each {|k, v|
+        @item[k] = v
+      }
+      if (@item.valid?) 
+        @item.save
+        {code: 1, desc: '保存成功！', id: @item.id}
+      else
+        {code: -1, desc: @item.errors.messages}
+      end
+
+      # search_field = this->search_field
+      # AdminLog::write(Auth::user()->username, $this->title . '添加' . $this->search_field . '=' . $item->$search_field, Request::getClientIp(), date('Y-m-d H:i:s', time()));
+    end
+
+    def update(params)
+      @item = self::find(params[:id])
+
+      params[self.model_name.singular].each {|k, v|
+        @item[k] = v
+      }
+      if (@item.valid?) 
+        @item.save
+        {code: 1, desc: '保存成功！', id: @item.id}
+      else
+        {code: -1, desc: @item.errors.messages}
+      end
+      # $search_field = $this->search_field;
+      # AdminLog::write(Auth::user()->username, $this->title . '编辑' . $this->search_field . '=' . $item->$search_field, Request::getClientIp(), date('Y-m-d H:i:s', time()));
+    end
+
+    def destroy(params) 
+      @item = self::find(params[:id])
+      @item.update(:is_deleted => 1)
+
+      {code: 1, desc: '删除成功！'}
+    end
+
+    def bulk_delete(params)
+      self.where(:id => params[:ids]).update_all(:is_deleted => 1)
+
+      {code: 1, desc: '删除成功！'}
     end
   end
 end
